af5eb70a0e98f6f468f93bf4a28dd06e7d716005
pholtz <pholtz@ycp.edu>
2013-12-16 22:02:10 -0500

    Create Lift.java

---
+++ Lift.java
@@ -0,0 +1,16 @@
+package edu.ycp.ece220.rgb;
+
+public class Lift {
+
+	public Point pos;
+	public Rectangle liftBlock;
+	public int startHeight, maxHeight;
+	
+	public Lift(Rectangle inBlock, int inMax) {
+		this.startHeight = (int)inBlock.topLeft.y;
+		this.liftBlock = inBlock;
+		this.pos = inBlock.topLeft;
+		this.maxHeight = inMax;
+	}
+
+}
