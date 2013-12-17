ffc2c0c14c1c9ee80bce2cbf6f08fe2d428625e7
pholtz <pholtz@ycp.edu>
2013-12-16 22:02:33 -0500

    Create Note.java

---
+++ Note.java
@@ -0,0 +1,13 @@
+package edu.ycp.ece220.rgb;
+
+public class Note {
+
+	public Point pos;
+	public String data;
+	
+	public Note(Point inPos, String inData) {
+		this.pos = inPos;
+		this.data = inData;
+	}
+
+}
